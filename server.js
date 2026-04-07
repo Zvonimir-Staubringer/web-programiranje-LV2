34cf496266597bf7d6b35017422931438078d13c
Zvonimir-Staubringer <zvonimir.staubringeros@gmail.com>
2026-04-07 14:49:02 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// set EJS
+app.set('view engine', 'ejs');
+
+// static files (VERY important)
 app.use(express.static('public'));
+
+app.get('/images', (req, res) => {
+  const images = [
+    { file: 'Batman.jpg', title: 'Batman' },
+    { file: 'La_La_Land_(film).png', title: 'La La Land' },
+    { file: 'forestgump.jpg', title: 'Forrest Gump' },
+    { file: 'captainamerica.jpg', title: 'Captain America' },
+    { file: 'soul.jpg', title: 'Soul' },
+    { file: 'harry_potter.jpg', title: 'Harry Potter' }
+  ];
+
+  res.render('images', { images });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
