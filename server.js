00d4e3ff5f557dfd1444293a6799571b0a147966
Zvonimir-Staubringer <zvonimir.staubringeros@gmail.com>
2026-04-07 14:09:34 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-res.send('Pozdrav sa Railway servera!');
-});
-app.listen(PORT, () => {
-console.log('Server pokrenut na portu ${PORT}');
-});
+app.use(express.static('public'));
